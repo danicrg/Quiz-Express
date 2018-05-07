90f04eaa246a6b049086ef8c18ff4d000daec838
Daniel Carlander <carlander.daniel@gmail.com>
2018-05-07 23:29:32 +0200

    2.1 - Inicialización de preguntas

--- models/quiz.js
+++ models/quiz.js
@@ -2,15 +2,15 @@
 // Definition of the Quiz model:
 
 module.exports = function (sequelize, DataTypes) {
-    return sequelize.define('quiz',{
-        question: {
-            type: DataTypes.STRING,
-            unique: {msg: "Ya existe esta pregunta"},
-            validate: {notEmpty: {msg: "La pregunta no puede estar vacía"}}
-        },
-        answer: {
-            type: DataTypes.STRING,
-            validate: {notEmpty: {msg: "La respuesta no puede estar vacía"}}
-        }
-    });
+    return sequelize.define('quiz',
+        {
+            question: {
+                type: DataTypes.STRING,
+                validate: {notEmpty: {msg: "Question must not be empty"}}
+            },
+            answer: {
+                type: DataTypes.STRING,
+                validate: {notEmpty: {msg: "Answer must not be empty"}}
+            }
+        });
 };
